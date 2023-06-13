9ff237914ca138799a1d432c4712be74b11867e4
verkesfl <floreverkest1@gmail.com>
2023-06-13 12:23:24 +0200

    first commit

---
+++ ragequiz/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rage(request):
+    return render(request, 'rage.html')
+
+def a(request):
+    return render(request, 'a.html')
+
+def b(request):
+    return render(request, 'b.html')
+
+def c(request):
+    return render(request, 'c.html')
+
+def d(request):
+    return render(request, 'd.html')
+
+def e(request):
+    return render(request, 'e.html')
+
+def f(request):
+    return render(request, 'f.html')
+
+def g(request):
+    return render(request, 'g.html')
+
+def h(request):
+    return render(request, 'h.html')
+
+def i(request):
+    return render(request, 'i.html')
+
+def j(request):
+    return render(request, 'j.html')
+
+def k(request):
+    return render(request, 'k.html')
+
+def l(request):
+    return render(request, 'l.html')
+
+def m(request):
+    return render(request, 'm.html')
+
+def n(request):
+    return render(request, 'n.html')
+
+def o(request):
+    return render(request, 'o.html')
+
+def r1(request):
+    return render(request, 'r1.html')
+
+def r2(request):
+    return render(request, 'r2.html')
+
+def r3(request):
+    return render(request, 'r3.html')
+
+def r4(request):
+    return render(request, 'r4.html')
+
+def r5(request):
+    return render(request, 'r5.html')
+
+def r6(request):
+    return render(request, 'r6.html')
+
+def r7(request):
+    return render(request, 'r7.html')
+
+def r8(request):
+    return render(request, 'r8.html')
+
+def r9(request):
+    return render(request, 'r9.html')
+
+def r10(request):
+    return render(request, 'r10.html')
+
+def r11(request):
+    return render(request, 'r11.html')
+
+def r12(request):
+    return render(request, 'r12.html')
+
+def r13(request):
+    return render(request, 'r13.html')
+
+def r14(request):
+    return render(request, 'r14.html')
+
+def r15(request):
+    return render(request, 'r15.html')
+
+def r16(request):
+    return render(request, 'r16.html')
